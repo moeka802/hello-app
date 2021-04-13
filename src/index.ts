f40ae06868ee2bc3d364847efd1a1dcd0495aa2b
moeka802 <42276835+moeka802@users.noreply.github.com>
2021-04-13 23:05:53 +0900

    常に最前面に表示する

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ const createWindow = (): void => {
   const mainWindow = new BrowserWindow({
     height: 600,
     width: 800,
+    alwaysOnTop: true // 常に最前面に表示する
   });
 
   // and load the index.html of the app.
